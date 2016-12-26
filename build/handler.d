d5e56b31decf2972bb8c18fbe7dd704fb45fea7d
Paul Oliver <paul.t.oliver.design@gmail.com>
2016-12-26 20:17:59 +0100

    Fixes to repo

--- build/handler.d
+++
@@ -1,6 +0,0 @@
-build/handler.o: src/handler.c include/tsalis.h \
- ..\..\libs\salis\include/salis.h ..\..\libs\salis\include/types.h \
- ..\..\libs\salis\include/instset.h ..\..\libs\salis\include/compiler.h \
- ..\..\libs\salis\include/loader.h ..\..\libs\salis\include/memory.h \
- ..\..\libs\salis\include/process.h ..\..\libs\salis\include/evolver.h \
- include/printer.h include/handler.h
